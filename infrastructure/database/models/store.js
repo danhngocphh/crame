011628868b46ab1292437c471973b7559de968d4
Vegarnom <danhngocphh@gmail.com>
2021-04-21 17:19:39 +0700

    [Setup] Update schema store

--- infrastructure/database/models/store.js
+++ infrastructure/database/models/store.js
@@ -25,44 +25,10 @@ const store = new mongoose.Schema({
             default: true,
         }
     }],
-    urlCallAPI: {
-        product: { type: String,  trim: true },
-        category: { type: String,  trim: true }
-    },
-    headers: {
-        type: Object,
-        required: true
-    },
-    params: {
-        type: Object,
-        required: true
-
-    },
     imgLogo: {
         type: String,
         lowercase: true,
         require: true
-    },
-    dataCallAPI:{
-        urlHeader: { type: String,  trim: true },
-        urlMiddle: { type: String,  trim: true },
-        urlFooter: { type: String,  trim: true },
-        urlProduct: { type: String,  trim: true },
-        imageProduct:{ type: String,  trim: true }
-    },
-    dataCrawlCategory:{
-        categoryPath: { type: String,  trim: true },
-        steps: [{
-            name: { type: String,  trim: true },
-            selector: { type: String,  trim: true }
-        }]
-    },
-    dataCrawlProduct:{
-        totalItem: { type: String,  trim: true },
-        image: { type: String,  trim: true },
-        name: { type: String,  trim: true },
-        detail: { type: String,  trim: true },
-        price: { type: String,  trim: true }
     },
     createdBy:{
         type: String
